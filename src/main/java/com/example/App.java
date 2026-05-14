54ecf623fd4cde345df845cd9f409bb88ffd4990
Sawez <Sawez@gmail.com>
2026-05-15 01:04:14 +0530

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Azure Pipeline CI/CD Project");
+
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Sum = " + (a + b));
+    }
+}
